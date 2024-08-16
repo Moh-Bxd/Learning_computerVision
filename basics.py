9d47c2398c998db44cce110f2b2faef62587985d
mohamed <m.rebiai@esi-sba.dz>
2024-08-16 16:46:55 +0100

    completed basics

--- basics.py
+++ basics.py
@@ -32,4 +32,22 @@ def webcam():
     webcam = cv2.VideoCapture(0)
     
     #visualize webcam
+    while True:
+        ret,f = webcam.read()
+        cv2.imshow('webcam', f)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    webcam.release()
+    cv2.destroyAllWindows()    
     
+while True:
+    print("1. Image\n2. Video\n3. Webcam\n4. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        image()
+    elif choice == 2:
+        video()
+    elif choice == 3:
+        webcam()
+    else:
+        break
